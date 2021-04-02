c057df6cb45f3e6da0c93a910438d2f391fc7091
JasonAmpec <wuzongye1@gmail.com>
2021-04-02 23:59:38 +1100

    8 port test sequence success

--- MCP23017.py
+++ MCP23017.py
@@ -2,6 +2,8 @@ import smbus
 import time
 
 flag = 1
+x = 0
+Output_adr = 0b00000001
  
 #bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
 bus = smbus.SMBus(1) # Rev 2 Pi uses 1
@@ -23,24 +25,27 @@ bus.write_byte_data(IN_DEVICE,IODIRA,0xff)
 bus.write_byte_data(IN_DEVICE,IODIRB,0xff)
 bus.write_byte_data(IN_DEVICE,GPPUA,0xff)
 bus.write_byte_data(IN_DEVICE,GPPUB,0xff)
+
+print ("Start Test Sequence")
  
 # Loop until user presses CTRL-C
 while True:
  
   # Read state of GPIOA register
-  if (flag == 1):
-    bus.write_byte_data(OUT_DEVICE,GPIOA,0xff)
-    bus.write_byte_data(OUT_DEVICE,GPIOB,0x00)
-    flag = 0
-  elif (flag == 0):
-    bus.write_byte_data(OUT_DEVICE,GPIOA,0x00)
-    bus.write_byte_data(OUT_DEVICE,GPIOB,0xff)   
-    flag = 1 
+  while (x <= 7): 
+    bus.write_byte_data(OUT_DEVICE,GPIOA,Output_adr)
+    Output_adr = Output_adr << 1
+    x = x + 1
 
-  Data = bus.read_byte_data(IN_DEVICE,GPIOA)
+    Data = bus.read_byte_data(IN_DEVICE,GPIOA)
+    print("Output:",format(Output_adr >> 1, '#010b'),"Readout",format(Data, '#010b'))
+    time.sleep(1)
+
+  x = 0
+  Output_adr = 0b00000001
   
-  print(bin(Data))
-  time.sleep(2)
+  print("**************END***************")
+  time.sleep(5)
 #TEST 1
 #TEST 2
 #Edit from webpage
