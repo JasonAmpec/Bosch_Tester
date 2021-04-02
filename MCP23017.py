b60857fb182830d8488ffcbb4a4bb14ef43ff308
JasonAmpec <wuzongye1@gmail.com>
2021-04-02 23:03:19 +1100

    Configured already

--- MCP23017.py
+++ MCP23017.py
@@ -1,26 +1,46 @@
 import smbus
 import time
+
+flag = 1
  
 #bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
 bus = smbus.SMBus(1) # Rev 2 Pi uses 1
  
-DEVICE = 0x20 # Device address (A0-A2)
+OUT_DEVICE = 0x20 # Device address (A0-A2)
+IN_DEVICE = 0X21
 IODIRA = 0x00 # Pin direction register
+IODIRB = 0x01
 GPIOA  = 0x12 # Register for inputs
+GPIOB = 0x13
+GPPUA = 0x0C
+GPPUB = 0x0D
  
 # Set first 7 GPA pins as outputs and
 # last one as input.
-bus.write_byte_data(DEVICE,IODIRA,0x80)
+bus.write_byte_data(OUT_DEVICE,IODIRA,0x00)
+bus.write_byte_data(OUT_DEVICE,IODIRB,0x00)
+bus.write_byte_data(IN_DEVICE,IODIRA,0xff)
+bus.write_byte_data(IN_DEVICE,IODIRB,0xff)
+bus.write_byte_data(IN_DEVICE,GPPUA,0xff)
+bus.write_byte_data(IN_DEVICE,GPPUB,0xff)
  
 # Loop until user presses CTRL-C
 while True:
  
   # Read state of GPIOA register
-  MySwitch = bus.read_byte_data(DEVICE,GPIOA)
- 
-  if MySwitch == 0b10000000:
-   print ('Switch was pressed')
-   time.sleep(1)
+  if (flag == 1):
+    bus.write_byte_data(OUT_DEVICE,GPIOA,0xff)
+    bus.write_byte_data(OUT_DEVICE,GPIOB,0x00)
+    flag = 0
+  elif (flag == 0):
+    bus.write_byte_data(OUT_DEVICE,GPIOA,0x00)
+    bus.write_byte_data(OUT_DEVICE,GPIOB,0xff)   
+    flag = 1 
+
+  Data = bus.read_byte_data(IN_DEVICE,GPIOA)
+  
+  print(bin(Data))
+  time.sleep(2)
 #TEST 1
 #TEST 2
 #Edit from webpage
